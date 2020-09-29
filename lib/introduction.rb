4bf2aa70c166c7e436ab222b37cdc163345ab7bc
ocawthorne <ocawthorne@gmail.com>
2020-09-29 01:56:31 +0100

    method-arguments-lab-092920

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+name = "Josh"
+lang = "Ember.js"
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+name = "Dan"
+
+def introduction_with_language(name, lang)
+    puts "Hi, my name is #{name} and I am learning to program in #{lang}.\n"
+end
